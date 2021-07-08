a857b06ccf1b5d9fbe46fd063619b1ef4e198896
Jack <jackdewinter1@gmail.com>
2021-07-08 23:56:46 +0100

    Initial commit

---
+++ prune/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using LibGit2Sharp;
+
+namespace prune
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deleting test branch...");
+            DeleteLocalBranch("test");
+        }
+
+        static void DeleteLocalBranch(string branchName)
+        {
+            using var repo = new Repository(Environment.CurrentDirectory);
+            var branch = repo.Branches.FirstOrDefault(b => b.FriendlyName == branchName);
+            repo.Branches.Remove(branch);
+        }
+    }
+}
